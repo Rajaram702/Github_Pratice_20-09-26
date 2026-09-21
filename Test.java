7f11b8394ea132aa9ec200ba2afa738ac6df0bb0
Razat <rjt@gmail.com>
2026-09-21 10:23:21 +0530

    m2 method added successfully..

--- Test.java
+++ Test.java
@@ -2,4 +2,7 @@ class Test{
     public static void main(String[] args){
         System.out.println("Test class updated by me...");
     }
+    public void m2(Integer[] arr){
+        Stream.of(arr).forEach(System.out::println);
+    }
 }
