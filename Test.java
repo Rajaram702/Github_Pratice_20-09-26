c16b923a27f3c16c3d233e7d51b2515fade959ea
Razat <rjt@gmail.com>
2026-09-21 10:26:06 +0530

    m1 method add successfully

--- Test.java
+++ Test.java
@@ -2,6 +2,11 @@ class Test{
     public static void main(String[] args){
         System.out.println("Test class updated by me...");
     }
+    public void m1(){
+        int var1=100;
+        int var2=200;
+        System.out.println(var1+var2);
+    }
     public void m2(Integer[] arr){
         Stream.of(arr).forEach(System.out::println);
     }
